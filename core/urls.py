be634a8965419eaeadfe0d859d47d794c6aa61c8
badalaryal11 <badal.aryal@gmail.com>
2025-12-01 00:05:48 +0545

    Implement custom login screen and logout functionality

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
